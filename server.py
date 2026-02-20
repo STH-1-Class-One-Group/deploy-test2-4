a1c99de0415b410ae4b82e5b25d9c48b240932eb
김수빈 <bz.subin@gmail.com>
2026-02-20 23:24:23 +0900

    서버 일단 저장

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
-import http.server
-import socketserver
+# 생각해보니 서버가 필요 없음..
 
-PORT = 8000
+from http.server import BaseHTTPRequestHandler, HTTPServer
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write("<h1>테스트</h1>".encode())
 
-Handler = http.server.SimpleHTTPRequestHandler
+host = "localhost"
+port = 8000
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+server = HTTPServer((host, port), MyHandler)
+print(f"http://{host}:{port}")
+
+try:
+    server.serve_forever()
+except:
+    server.server_close()
+
